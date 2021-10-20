406ccb71cb6dc1fa6acb547ca8c5622b570d7816
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-10-20 18:42:12 +0530

    auth middleware

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use(logger);
+app.use(logger); // global middleware
 
 app.get("/", (req, res) => {
     res.send("Home Page");
 })
 
-app.get("/users", (req, res) => {
+app.get("/users", auth, (req, res) => {
     res.send("Users Page");
 })
 
+
 function logger(req, res, next) {
-    console.log('log');
+    console.log('Auth');
     next();
+}
+
+function auth(req, res, next) {
+    if (req.query.admin === 'true') {
+        next();
+    }
+    else {
+        res.send('No auth');
+    }
 }
 
 app.listen(3000);
