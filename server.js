a1c399525da0c7b955371f0d724d37a4cbab4acc
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-10-20 15:46:29 +0530

    created routes

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
+
+app.get("/users", (req, res) => {
+    res.send("Users Page");
+})
+
+function logger(req, res, next) {
+    console.log('log');
+}
+
+app.listen(3000);
